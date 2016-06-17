ed8bcd58930ff397e710e90127b3c1a8ad6d45d1
Joel Waage <joeltwaage@gmail.com>
2016-06-17 14:25:34 -0700

    add click hide/show for third and fourth survey questions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
-  $("#submit").click(function() {
+  $("#submit1").click(function() {
     $(".question1").hide();
     $(".question2").show();
   });
+
+  $("#submit2").click(function() {
+    $(".question2").hide();
+    $(".question3").show();
+  });
+
+  $("#submit3").click(function() {
+    $(".question3").hide();
+    $(".question4").show();
+  });
+
+  $("#submit4").click(function() {
+    $(".question4").hide();
+    $(".question5").show();
+  });
+
+
+
 });
