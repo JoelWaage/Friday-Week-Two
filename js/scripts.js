da244451cd639fabf566e82badee986f886aa345
Joel Waage <joeltwaage@gmail.com>
2016-06-17 16:24:50 -0700

    scrolling questions with limited functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,64 @@
-
-
-
-
-
-
-
-
-
-
 $(document).ready(function() {
-  $("#submit1").click(function() {
+  $("#blanks form").submit(function() {
+    var personName = $("input#personName").val();
+    var email = $("input#userEmail").val();
+    var what = $("input#what").val();
+    var meat = $("input:radio[name=meat]:checked").val();
+    var fire = $("input:radio[name=fire]:checked").val();
+    var electricity = $("#electricity").val();
+
+    $(".personName").append(personName).val();
+
+    alert(personName);
+    alert(email);
+    alert(what);
+    alert(meat);
+    alert(fire);
+    alert(electricity);
+
+
+      });
+
+
+
+
+
+// $(document).ready(function() {
+  $("#submit1").click(function(event) {
     $(".question1").hide();
     $(".question2").show();
+    event.preventDefault();
   });
 
-  $("#submit2").click(function() {
+  $("#submit2").click(function(event) {
     $(".question2").hide();
     $(".question3").show();
+    event.preventDefault();
   });
 
-  $("#submit3").click(function() {
+  $("#submit3").click(function(event) {
     $(".question3").hide();
     $(".question4").show();
+    event.preventDefault();
   });
 
-  $("#submit4").click(function() {
+  $("#submit4").click(function(event) {
     $(".question4").hide();
     $(".question5").show();
+    event.preventDefault();
   });
 
-  $("#submit5").click(function() {
+  $("#submit5").click(function(event) {
     $(".question5").hide();
     $(".question6").show();
+    event.preventDefault();
   });
 
-  $("#submit6").click(function() {
+  $("#submit6").click(function(event) {
     $(".question6").hide();
     $(".vacationResult").show();
+    event.preventDefault();
   });
+
 
 });
