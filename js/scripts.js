40d18222493ca42354f146a73caac76ae92e517f
Joel Waage <joeltwaage@gmail.com>
2016-06-20 21:21:54 -0700

    correct variable assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,30 @@
+var personName = '';
+var email = '';
+var what = '';
+var meat = '';
+var fire = '';
+var electricty = '';
+var lizards = '';
+
+
 $(document).ready(function() {
-  $("#blanks form").submit(function() {
-    var personName = $("input#personName").val();
-    var email = $("input#userEmail").val();
-    var what = $("input#what").val();
-    var meat = $("input:radio[name=meat]:checked").val();
-    var fire = $("input:radio[name=fire]:checked").val();
-    var electricity = $("#electricity").val();
+  $("#blanks form").submit(function(event) {
+    event.preventDefault();
+    personName = $("input#personName").val();
+    email = $("input#userEmail").val();
+    what = $("input#what").val();
+    meat = $("input:radio[name=meat]:checked").val();
+    fire = $("input:radio[name=fire]:checked").val();
+    electricity = $("input:radio[name=electricity]:checked").val();
+    lizards = $("#lizards").val();
 
-    if (meat === "love") {
-      if (fire === "fireYes") {
-        alert("You should go to the Mezozoic Era");
-      } else if (fire === "fireMaybe") {
-        alert("You should go to Ancient France with early Man")''
-      } else if (fire === "fireNo") {
-        alert("You should got to Medieval Europe");
-      }
-
-    }
-
-
-
-    $(".personName").append(personName).val();
-
-    // alert(personName);
-    // alert(email);
-    // alert(what);
-    // alert(meat);
-    // alert(fire);
-    // alert(electricity);
-
+    alert(personName);
+    alert(email);
+    alert(what);
+    alert(meat);
+    alert(fire);
+    alert(electricity);
+    alert(lizards);
 
   });
 });
